95caaac33c59fb504ca44ff3364620105e40a8d6
venonh2 <brendow-evan@hotmail.com>
2019-10-20 13:34:35 -0300

    instanciado o curriculo

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ const path = require('path');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.get('/',function(req, res, next){
+
+app.get('/', function (req, res, next) {
     res.render('index', { // antes estava sender, ou seja só enviando dados para o servidor, agora o renderdiz que vamos renderimas dados (html)
         title: "Meu primeiro servidor express",
         version: "0.0.0"
@@ -15,8 +16,29 @@ app.get('/',function(req, res, next){
 
 });
 
+
 app.listen(port, err => {
     console.log(`Servidor is listening on ${port}`);
+});
+
+app.get('/curriculo', (req, res, next) => {
+    res.render('curriculo', {
+        title: 'Meu curriculo',
+        name: 'Brendow E. R',
+        profession: 'Software Engineer',
+        description: 'Aprendendo js',
+        experience: [{
+            company: 'Uepg',
+            office: 'Eng de Software',
+            descrition: '# an faculdade'
+        },
+        {
+            company: 'prefeitura',
+            office: 'hc',
+            description: 'manutenção ..'
+        }],
+        skills: ['backend','frontend','manutenção']
+    });
 });
 
 

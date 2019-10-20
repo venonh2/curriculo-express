2af34c945f43782eca773e7d8680f0e280181b68
venonh2 <brendow-evan@hotmail.com>
2019-10-20 14:44:40 -0300

    deletado pasta node_modules para testes.

--- app.js
+++ app.js
@@ -8,10 +8,10 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 
-app.get('/', function (req, res, next) {
+app.get('/', function(req, res, next){
     res.render('index', { // antes estava sender, ou seja só enviando dados para o servidor, agora o renderdiz que vamos renderimas dados (html)
         title: "Meu primeiro servidor express",
-        version: "0.0.0"
+        version: "0.0.0",
     });
 
 });
